15224933e0474eee70ecfc16c7f7bb2607f9b89d
swati-iitm <23ds3000185@ds.study.iitm.ac.in>
2025-02-02 20:25:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     file_path = 'q-vercel-python.json'  # JSON file path
-    name_filter=request.args.getlist('name')
+    #name_filter=request.args.getlist('name')
     return file_path
     #if not name_filter:
      #   return 'No names passed'
