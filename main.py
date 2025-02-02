1bad9c04d571da1e7a54df65c6f8286068d77bef
swati-iitm <23ds3000185@ds.study.iitm.ac.in>
2025-02-02 22:37:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ async def get_students(name: Optional[List[str]] = Query(None)):
     #return(f"Requested names: {name}")  # Debugging line
     if name:
         #filtered_students = [student for student in students if student["name"] in name_]
-        filtered_students = [entry['marks'] for entry in data_1 if entry['name'] in name] 
+        filtered_students = [entry for entry in data_1 if entry['name'] in name] 
         print(f"Filtered students: {filtered_students}")  # Debugging line
         return {"marks": filtered_students}
     return {"marks": students}
