58d982fe10ec06c282ce519d5e4690299c063e89
swati-iitm <23ds3000185@ds.study.iitm.ac.in>
2025-02-02 20:24:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ app = FastAPI()
 def read_root():
     file_path = 'q-vercel-python.json'  # JSON file path
     name_filter=request.args.getlist('name')
-    if not name_filter:
-        return 'No names passed'
-    with open(file_path, 'r') as file:
-        data_1=json.load(file)
-    filtered_data = [entry['marks'] for entry in data_1 if entry['name'] in name_filter] 
-    return {"marks": "Hello, World!"}
+    return file_path
+    #if not name_filter:
+     #   return 'No names passed'
+    #with open(file_path, 'r') as file:
+     #   data_1=json.load(file)
+    #filtered_data = [entry['marks'] for entry in data_1 if entry['name'] in name_filter] 
+    #return {"marks": "Hello, World!"}
